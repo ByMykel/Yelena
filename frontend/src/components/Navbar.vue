df15b48b077b2977721dd767039af16bf293e4e3
ByMykel <bymykel5@gmail.com>
2021-08-06 21:29:35 +0200

    feat: add app layout and navbar components.

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="bg-gray-800">
+        <nav class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div class="flex py-2">
+                <router-link
+                    to="/"
+                    class="block px-3 py-2 rounded-md text-sm font-medium"
+                    :class="
+                        $route.name === 'Home'
+                            ? 'bg-gray-900 text-white'
+                            : 'text-gray-300 hover:bg-black-200 hover:text-white'
+                    "
+                    >Home</router-link
+                >
+
+                <router-link
+                    to="/cards"
+                    class="block px-3 py-2 rounded-md text-sm font-medium"
+                    :class="
+                        $route.name === 'Cards'
+                            ? 'bg-gray-900 text-white'
+                            : 'text-gray-300 hover:bg-black-200 hover:text-white'
+                    "
+                    >Cards</router-link
+                >
+
+                <router-link
+                    to="/decks"
+                    class="block px-3 py-2 rounded-md text-sm font-medium"
+                    :class="
+                        $route.name === 'Decks'
+                            ? 'bg-gray-900 text-white'
+                            : 'text-gray-300 hover:bg-black-200 hover:text-white'
+                    "
+                    >Decks</router-link
+                >
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Navbar></Navbar>
+        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+export default {
+    components: { Navbar },
+};
+</script>
