9ef7244b1863d7e94e120b813611a87ddf6c8f04
ByMykel <bymykel5@gmail.com>
2021-08-07 22:13:36 +0200

    feat: add 404 page.

---
+++ frontend/src/views/PageNotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex items-center justify-center text-6xl font-medium mt-10">
+        <div>
+            <span class="text-purple-600 mr-5">404</span>
+            <span class="text-gray-800">Page not Found</span>
+        </div>
+    </div>
+</template>
