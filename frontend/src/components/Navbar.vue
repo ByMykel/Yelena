2dc0b1509589a70dadf9b92c5c34d19f066125a2
ByMykel <bymykel5@gmail.com>
2021-08-09 23:40:59 +0200

    feat: add button to create new decks.

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,29 +1,65 @@
 <template>
     <div class="bg-gray-800">
-        <nav class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <div class="flex py-2">
-                <router-link
-                    to="/"
-                    class="block px-3 py-2 rounded-md text-sm font-medium"
-                    :class="
-                        $route.name === 'Home'
-                            ? 'bg-gray-900 text-white'
-                            : 'text-gray-300 hover:bg-black-200 hover:text-white'
-                    "
-                    >Home</router-link
-                >
-                <router-link
-                    to="/decks"
-                    class="block px-3 py-2 rounded-md text-sm font-medium"
-                    :class="
-                        $route.name === 'Decks'
-                            ? 'bg-gray-900 text-white'
-                            : 'text-gray-300 hover:bg-black-200 hover:text-white'
-                    "
-                    >Decks</router-link
-                >
+        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div class="flex justify-between py-2 items-center">
+                <nav>
+                    <div class="flex">
+                        <router-link
+                            to="/"
+                            class="
+                                block
+                                px-3
+                                py-2
+                                rounded-md
+                                text-sm
+                                font-medium
+                            "
+                            :class="
+                                $route.name === 'Home'
+                                    ? 'bg-gray-900 text-white'
+                                    : 'text-gray-300 hover:bg-black-200 hover:text-white'
+                            "
+                            >Home</router-link
+                        >
+                        <router-link
+                            to="/decks"
+                            class="
+                                block
+                                px-3
+                                py-2
+                                rounded-md
+                                text-sm
+                                font-medium
+                            "
+                            :class="
+                                $route.name === 'Decks'
+                                    ? 'bg-gray-900 text-white'
+                                    : 'text-gray-300 hover:bg-black-200 hover:text-white'
+                            "
+                            >Decks</router-link
+                        >
+                    </div>
+                </nav>
+                <div>
+                    <button
+                        class="
+                            flex
+                            items-center
+                            jutify-center
+                            bg-green-400
+                            hover:bg-green-500
+                            p-1
+                            rounded
+                            text-sm
+                            font-medium
+                        "
+                        @click="$emit('create-deck')"
+                    >
+                        Create deck
+                    </button>
+                </div>
             </div>
-        </nav>
+        </div>
     </div>
 </template>
 
