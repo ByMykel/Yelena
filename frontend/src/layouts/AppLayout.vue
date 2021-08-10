f32f3c32684093ae6cf2eaa1e5967ab58ffd6f7a
ByMykel <bymykel5@gmail.com>
2021-08-10 21:46:35 +0200

    feat: add Modal with form to create decks.

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <Navbar></Navbar>
+        <Modal :show="show" @close-modal="show = false">
+            <CreateDeck></CreateDeck>
+        </Modal>
+        <Navbar @create-deck="show = true"></Navbar>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <slot></slot>
         </div>
@@ -8,8 +11,15 @@
 </template>
 
 <script>
+import Modal from "../components/Modal.vue";
 import Navbar from "../components/Navbar.vue";
+import CreateDeck from "../components/CreateDeck.vue";
 export default {
-    components: { Navbar },
+    components: { Navbar, Modal, CreateDeck },
+    data() {
+        return {
+            show: false,
+        };
+    },
 };
 </script>
