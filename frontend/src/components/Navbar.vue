b0e5923e4bd5179a1338261ec70aee538c0c8c78
ByMykel <bymykel5@gmail.com>
2021-08-11 23:46:06 +0200

    refactor: change navbar design.

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -6,56 +6,84 @@
                     <div class="flex">
                         <router-link
                             to="/"
-                            class="
-                                block
-                                px-3
-                                py-2
-                                rounded-md
-                                text-sm
-                                font-medium
-                            "
+                            class="block px-3 py-2 rounded-md font-medium"
                             :class="
                                 $route.name === 'Home'
-                                    ? 'bg-gray-900 text-white'
-                                    : 'text-gray-300 hover:bg-black-200 hover:text-white'
+                                    ? 'text-blue-500'
+                                    : 'text-white hover:text-gray-300'
                             "
                             >Home</router-link
                         >
                         <router-link
                             to="/decks"
-                            class="
-                                block
-                                px-3
-                                py-2
-                                rounded-md
-                                text-sm
-                                font-medium
-                            "
+                            class="block px-3 py-2 rounded-md font-medium"
                             :class="
                                 $route.name === 'Decks'
-                                    ? 'bg-gray-900 text-white'
-                                    : 'text-gray-300 hover:bg-black-200 hover:text-white'
+                                    ? 'text-blue-500'
+                                    : 'text-white hover:text-gray-300'
                             "
                             >Decks</router-link
                         >
                     </div>
                 </nav>
-                <div>
+                <div class="flex space-x-2">
                     <button
                         class="
                             flex
                             items-center
                             jutify-center
-                            bg-green-400
-                            hover:bg-green-500
-                            p-1
+                            bg-gray-600 bg-opacity-50
+                            hover:bg-opacity-75
+                            px-2
+                            py-2
                             rounded
-                            text-sm
+                            text-sm text-white
                             font-medium
                         "
                         @click="$emit('create-deck')"
                     >
-                        Create deck
+                        <svg
+                            class="w-5 h-5"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                                clip-rule="evenodd"
+                            ></path>
+                        </svg>
+                        <span class="mx-1">Deck</span>
+                    </button>
+                    <button
+                        class="
+                            flex
+                            items-center
+                            jutify-center
+                            bg-gray-600 bg-opacity-50
+                            hover:bg-opacity-75
+                            px-2
+                            py-2
+                            rounded
+                            text-sm text-white
+                            font-medium
+                        "
+                        @click="$emit('create-card')"
+                    >
+                        <svg
+                            class="w-5 h-5"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                                clip-rule="evenodd"
+                            ></path>
+                        </svg>
+                        <span class="mx-1">Card</span>
                     </button>
                 </div>
             </div>
