27cf0d093f3406fac24f2b69e83842e75555b1af
ByMykel <bymykel5@gmail.com>
2021-08-15 22:55:36 +0200

    feat: add study module.

---
+++ frontend/src/store/modules/study.js
@@ -0,0 +1,27 @@
+import repository from "../../api/repository";
+
+export const namespaced = true;
+
+export const state = {
+    study_deck: null,
+};
+
+export const mutations = {
+    SET_STUDY_DECK(state, deck) {
+        state.study_deck = deck;
+    },
+};
+
+export const actions = {
+    async fetchStudyDeck({ commit }, id) {
+        await repository.getStudyDeck(id).then((data) => {
+            commit("SET_STUDY_DECK", data.data.data);
+        });
+    },
+};
+
+export const getters = {
+    getStudyDeck(state) {
+        return state.study_deck;
+    },
+};
