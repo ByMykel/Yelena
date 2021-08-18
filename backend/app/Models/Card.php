2a20ca1438f6c302c3b031bed5b40fe3565735e1
ByMykel <bymykel5@gmail.com>
2021-08-18 20:51:42 +0200

    feat: add human date for review.

--- backend/app/Models/Card.php
+++ backend/app/Models/Card.php
@@ -11,14 +11,31 @@ class Card extends Model
 
     protected $guarded = [];
 
+    protected $appends = ['review_date_human'];
+
     protected $casts = [
         'repetitions' => 'integer',
         'ease_factor' => 'float',
         'interval' => 'integer',
     ];
 
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'review_date'
+    ];
+
     public function deck()
     {
         return $this->belongsTo(Deck::class);
+    }
+
+    public function getReviewDateHumanAttribute()
+    {
+        if ($this->review_date) {
+            return $this->review_date->diffForHumans();
+        }
+
+        return "";
     }
 }
