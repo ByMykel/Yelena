3cbac2afec8c8338f3270c393fef0ed76471f441
ByMykel <bymykel5@gmail.com>
2021-08-20 23:44:18 +0200

    refactor: improve form to create decks.

--- frontend/src/components/CreateDeck.vue
+++ frontend/src/components/CreateDeck.vue
@@ -1,54 +1,31 @@
 <template>
-    <Modal :show="show">
-        <div class="grid grid-cols-3 gap-6">
-            <div class="col-span-3 sm:col-span-2">
-                <label
-                    for="deck-name"
-                    class="block text-sm font-medium text-gray-700"
-                    >Deck name</label
-                >
-                <input
-                    id="deck-name"
-                    v-model="deckForm.name"
-                    type="text"
-                    class="
-                        focus:ring-indigo-500
-                        focus:border-indigo-500
-                        flex-1
-                        block
-                        w-full
-                        rounded-md
-                        sm:text-sm
-                        border-gray-300
-                    "
-                    placeholder="Vocabulary - Spanish to English"
-                />
-            </div>
+    <modal :show="show">
+        <div class="px-4 py-4">
+            <label
+                for="deck-name"
+                class="block text-sm font-medium text-gray-700"
+                >Deck name</label
+            >
+            <input
+                id="deck-name"
+                v-model="deckForm.name"
+                type="text"
+                class="flex-1 block w-full mt-1 border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                placeholder="Vocabulary - Spanish to English"
+            />
         </div>
-        <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+
+        <div
+            class="px-4 py-3 text-right border-t border-gray-200 bg-gray-50 sm:px-6"
+        >
             <button
-                class="
-                    inline-flex
-                    justify-center
-                    py-2
-                    px-4
-                    border border-transparent
-                    shadow-sm
-                    text-sm
-                    font-medium
-                    rounded-md
-                    text-white
-                    bg-indigo-600
-                    hover:bg-indigo-700
-                    focus:outline-none
-                    focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
-                "
+                class="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                 @click="handleForm()"
             >
                 Save
             </button>
         </div>
-    </Modal>
+    </modal>
 </template>
 
 <script>
