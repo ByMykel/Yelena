1c191063bd7127b95a867a72eda01474d71dcf2c
ByMykel <bymykel5@gmail.com>
2021-08-21 23:35:23 +0200

    refactor: change some icons.

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="bg-gray-800">
-        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <div class="flex justify-between py-2 items-center">
+        <div class="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="flex items-center justify-between py-2">
                 <nav>
                     <div class="flex">
                         <router-link
                             to="/"
-                            class="block px-3 py-2 rounded-md font-medium"
+                            class="block px-3 py-2 font-medium rounded-md"
                             :class="
                                 $route.name === 'Home'
                                     ? 'text-blue-500'
@@ -16,7 +16,7 @@
                         >
                         <router-link
                             to="/decks"
-                            class="block px-3 py-2 rounded-md font-medium"
+                            class="block px-3 py-2 font-medium rounded-md"
                             :class="
                                 $route.name === 'Decks'
                                     ? 'text-blue-500'
@@ -28,88 +28,61 @@
                 </nav>
                 <div class="flex space-x-2">
                     <button
-                        class="
-                            flex
-                            items-center
-                            jutify-center
-                            bg-gray-600 bg-opacity-50
-                            hover:bg-opacity-75
-                            px-2
-                            py-2
-                            rounded
-                            text-sm text-white
-                            font-medium
-                        "
+                        class="flex items-center px-2 py-2 text-sm font-medium text-white bg-gray-600 bg-opacity-50 rounded jutify-center hover:bg-opacity-75"
                         @click="$emit('create-deck')"
                     >
                         <svg
                             class="w-5 h-5"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg"
                         >
                             <path
-                                fill-rule="evenodd"
-                                d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                                clip-rule="evenodd"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
                             ></path>
                         </svg>
                         <span class="mx-1">Deck</span>
                     </button>
                     <button
-                        class="
-                            flex
-                            items-center
-                            jutify-center
-                            bg-gray-600 bg-opacity-50
-                            hover:bg-opacity-75
-                            px-2
-                            py-2
-                            rounded
-                            text-sm text-white
-                            font-medium
-                        "
+                        class="flex items-center px-2 py-2 text-sm font-medium text-white bg-gray-600 bg-opacity-50 rounded jutify-center hover:bg-opacity-75"
                         @click="$emit('create-card')"
                     >
                         <svg
                             class="w-5 h-5"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg"
                         >
                             <path
-                                fill-rule="evenodd"
-                                d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                                clip-rule="evenodd"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
                             ></path>
                         </svg>
                         <span class="mx-1">Card</span>
                     </button>
                     <button
-                        class="
-                            flex
-                            items-center
-                            jutify-center
-                            bg-gray-600 bg-opacity-50
-                            hover:bg-opacity-75
-                            px-2
-                            py-2
-                            rounded
-                            text-sm text-white
-                            font-medium
-                        "
+                        class="flex items-center px-2 py-2 text-sm font-medium text-white bg-gray-600 bg-opacity-50 rounded jutify-center hover:bg-opacity-75"
                         @click="$emit('import-card')"
                     >
                         <svg
                             class="w-5 h-5"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg"
                         >
                             <path
-                                fill-rule="evenodd"
-                                d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                                clip-rule="evenodd"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
                             ></path>
                         </svg>
                         <span class="mx-1">Import Cards</span>
