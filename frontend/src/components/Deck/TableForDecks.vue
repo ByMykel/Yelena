1e85c1ad8be7aa7d71c905e5a92e789e08d01be8
ByMykel <bymykel5@gmail.com>
2021-09-02 19:11:03 +0200

    refactor: change span for button.

--- frontend/src/components/Deck/TableForDecks.vue
+++ frontend/src/components/Deck/TableForDecks.vue
@@ -1,10 +1,10 @@
 <template>
     <table class="min-w-full divide-y divide-gray-200">
-        <thead class="bg-gray-50">
+        <thead class="bg-white">
             <tr>
                 <th
                     scope="col"
-                    class="py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase  px-7"
+                    class="py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase px-7"
                 >
                     Name
                 </th>
@@ -30,17 +30,17 @@
                 :class="[deck.due_cards_count == 0 ? 'bg-gray-50' : 'bg-white']"
             >
                 <td
-                    class="relative py-4 text-sm font-medium text-gray-900  px-7 whitespace-nowrap"
+                    class="relative py-4 text-sm font-medium text-gray-900 px-7 whitespace-nowrap"
                 >
                     <div
-                        class="absolute inset-0 flex items-center justify-center  w-7"
+                        class="absolute inset-0 flex items-center justify-center w-7"
                     >
-                        <span
+                        <button
                             v-if="deck.favorite"
                             @click="handleFavorite(deck)"
                         >
                             <svg
-                                class="w-5 h-5 text-yellow-300 cursor-pointer  hover:text-yellow-200"
+                                class="w-5 h-5 text-yellow-300 cursor-pointer hover:text-yellow-200"
                                 fill="currentColor"
                                 viewBox="0 0 20 20"
                                 xmlns="http://www.w3.org/2000/svg"
@@ -49,10 +49,10 @@
                                     d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
                                 ></path>
                             </svg>
-                        </span>
-                        <span v-else @click="handleFavorite(deck)">
+                        </button>
+                        <button v-else @click="handleFavorite(deck)">
                             <svg
-                                class="w-5 h-5 text-gray-300 cursor-pointer  hover:text-yellow-300"
+                                class="w-5 h-5 text-gray-300 cursor-pointer hover:text-yellow-300"
                                 fill="none"
                                 stroke="currentColor"
                                 viewBox="0 0 24 24"
@@ -65,7 +65,7 @@
                                     d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
                                 ></path>
                             </svg>
-                        </span>
+                        </button>
                     </div>
                     <span
                         :class="{
@@ -91,14 +91,11 @@
                     {{ deck.due_cards_count }}
                 </td>
                 <td
-                    class="flex items-center justify-end px-6 py-4 text-sm font-medium text-right  whitespace-nowrap"
+                    class="flex items-center justify-end px-6 py-4 text-sm font-medium text-right whitespace-nowrap"
                 >
                     <button
-                        class="mr-4 font-semibold text-gray-900 cursor-pointer  hover:text-gray-600"
-                        :class="{
-                            'opacity-50 cursor-not-allowed':
-                                deck.due_cards_count == 0,
-                        }"
+                        class="mr-4 font-semibold text-gray-900 cursor-pointer hover:text-gray-600 disabled:opacity-50 disabled:cursor-not-allowed"
+                        :disabled="deck.due_cards_count == 0"
                         title="Study this deck"
                         @click="studySelectedDeck(deck)"
                     >
@@ -126,7 +123,7 @@
                     <router-link
                         :to="`/decks/${deck.id}`"
                         title="Check Cards"
-                        class="mr-4 font-semibold text-gray-900 cursor-pointer  hover:text-gray-600"
+                        class="mr-4 font-semibold text-gray-900 cursor-pointer hover:text-gray-600"
                     >
                         <svg
                             class="w-5 h-5"
@@ -145,7 +142,7 @@
                     </router-link>
                     <button
                         title="Delete deck"
-                        class="ml-3 font-semibold text-red-600 cursor-pointer  hover:text-red-900"
+                        class="ml-3 font-semibold text-red-600 cursor-pointer hover:text-red-900"
                     >
                         <svg
                             class="w-5 h-5"
