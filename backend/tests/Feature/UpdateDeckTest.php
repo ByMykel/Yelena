58d685967be46cbc497960739a61c2dda8edd972
ByMykel <bymykel5@gmail.com>
2021-09-20 23:58:45 +0200

    test: test whether deck is updated successfully.

---
+++ backend/tests/Feature/UpdateDeckTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Deck;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+class UpdateDeckTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_deck_is_updated_successfully()
+    {
+        $deck = Deck::factory()->create([
+            'name' => 'test name'
+        ]);
+
+        $response = $this->put("api/decks/$deck->id/update", [
+            'name' => 'new test name'
+        ]);
+
+        $response->assertStatus(Response::HTTP_OK);
+
+        $this->assertEquals($deck->fresh()->name, 'new test name');
+    }
+}
