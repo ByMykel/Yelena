b0928b0f05551031d7261d027a85fda53975765b
ByMykel <bymykel5@gmail.com>
2021-09-21 13:33:56 +0200

    feat: add supermemo service.

---
+++ backend/app/Services/SuperMemo.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+class SuperMemo
+{
+    public function calculate($quality, $oldInterval, $oldRepetitions, $oldEaseFactor)
+    {
+        $interval = 0;
+        $repetitions = 0;
+        $easeFactor = 0;
+
+        if ($quality >= 3) {
+            switch ($oldRepetitions) {
+                case 0:
+                    $interval = 1;
+                    break;
+                case 1:
+                    $interval = 6;
+                    break;
+                default:
+                    $interval = round($oldInterval * $oldEaseFactor);
+            }
+
+            $repetitions = $oldRepetitions + 1;
+        } else {
+            $repetitions = 0;
+            $interval = 1;
+        }
+
+        $easeFactor = $oldEaseFactor + (0.1 - (5 - $quality) * (0.08 + (5 - $quality) * 0.02));
+        $easeFactor = max($easeFactor, 1.3);
+
+        return [
+            'interval' => $interval,
+            'repetitions' => $repetitions,
+            'easeFactor' => $easeFactor
+        ];
+    }
+}
