fce0132b6763ceb33c58e085dcb9e251cbb7f0de
ByMykel <bymykel5@gmail.com>
2021-09-27 19:11:11 +0200

    refactor: use heroicons component.

--- frontend/src/components/Deck/ActionDeleteDeck.vue
+++ frontend/src/components/Deck/ActionDeleteDeck.vue
@@ -5,50 +5,35 @@
     >
         <div class="flex flex-row items-center justify-center h-full">
             <div
-                class="transition duration-300 ease-out transform scale-0 bg-transparent bg-red-600 rounded-full opacity-0 group-hover:opacity-100 group-focus:opacity-100 group-hover:scale-100 group-focus:scale-100 w-7 h-7 bg-opacity-20"
+                class="transition duration-300 ease-out transform scale-0 bg-transparent bg-red-600 rounded-full opacity-0  group-hover:opacity-100 group-focus:opacity-100 group-hover:scale-100 group-focus:scale-100 w-7 h-7 bg-opacity-20"
             ></div>
         </div>
         <div class="absolute inset-0">
             <div
-                class="flex items-center justify-center w-full h-full text-gray-900 group-hover:text-red-600 group-focus:text-red-600"
+                class="flex items-center justify-center w-full h-full text-gray-900  group-hover:text-red-600 group-focus:text-red-600"
             >
-                <svg
-                    v-if="confirmed"
-                    title="Confirm to delete this deck"
-                    class="w-5 h-5 text-red-600"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        fill-rule="evenodd"
-                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
-                        clip-rule="evenodd"
-                    ></path>
-                </svg>
-                <svg
-                    v-else
-                    title="Delete this deck"
+                <hero-icons-outline
+                    v-if="!confirmed"
                     class="w-5 h-5"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                    ></path>
-                </svg>
+                    title="Confirm to delete this deck"
+                    name="trash"
+                ></hero-icons-outline>
+                <hero-icons-solid
+                    v-else
+                    class="w-5 h-5 text-red-600"
+                    title="Delete this deck"
+                    name="exclamation-circle"
+                ></hero-icons-solid>
             </div>
         </div>
     </button>
 </template>
 
 <script>
+import HeroIconsOutline from "../HeroIconsOutline.vue";
+import HeroIconsSolid from "../HeroIconsSolid.vue";
 export default {
+    components: { HeroIconsOutline, HeroIconsSolid },
     props: {
         deckId: Number,
     },
--- frontend/src/components/Deck/TableForDecks.vue
+++ frontend/src/components/Deck/TableForDecks.vue
@@ -100,46 +100,20 @@
                         @hover-icon="studySelectedDeck(deck)"
                         @click-icon="studySelectedDeck(deck)"
                     >
-                        <svg
+                        <hero-icons-outline
                             class="w-5 h-5"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-                            ></path>
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                            ></path>
-                        </svg>
+                            name="play"
+                        ></hero-icons-outline>
                     </icon-background>
                     <icon-background
                         class="mr-2"
                         title="Check Cards"
                         @click-icon="visitCardsPage(deck)"
                     >
-                        <svg
+                        <hero-icons-outline
                             class="w-5 h-5"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-                            ></path>
-                        </svg>
+                            name="external-link"
+                        ></hero-icons-outline>
                     </icon-background>
                     <action-delete-deck
                         :deck-id="deck.id"
@@ -156,12 +130,18 @@ import { mapGetters } from "vuex";
 import repository from "../../api/repository";
 import router from "../../router";
 import store from "../../store";
+import HeroIconsOutline from "../HeroIconsOutline.vue";
 import IconBackground from "../IconBackground.vue";
 import ActionDeleteDeck from "./ActionDeleteDeck.vue";
 import ActionUpdateDeck from "./ActionUpdateDeckName.vue";
 
 export default {
-    components: { ActionDeleteDeck, ActionUpdateDeck, IconBackground },
+    components: {
+        ActionDeleteDeck,
+        ActionUpdateDeck,
+        IconBackground,
+        HeroIconsOutline,
+    },
     computed: {
         ...mapGetters("deck", ["getDecks"]),
     },
