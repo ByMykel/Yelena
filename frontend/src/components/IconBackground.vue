63f39d5735262aa1e6949169e7dd32cb0326409e
ByMykel <38622893+ByMykel@users.noreply.github.com>
2021-09-29 22:22:02 +0200

    refactor: delete keydown enter and add ability to choose color.

--- frontend/src/components/IconBackground.vue
+++ frontend/src/components/IconBackground.vue
@@ -3,22 +3,22 @@
         class="relative block focus:outline-none group"
         :class="{ 'cursor-not-allowed opacity-50': disabledIcon }"
         :disabled="disabledIcon"
-        @keydown.enter="$emit('hover-icon')"
         @click="$emit('click-icon')"
     >
         <div class="flex flex-row items-center justify-center h-full">
             <div
-                class="transition duration-300 ease-out transform scale-0 rounded-full opacity-0 group-hover:opacity-100 group-focus:opacity-100 group-hover:scale-100 group-focus:scale-100 w-7 h-7"
-                :class="{ 'bg-transparent bg-blue-600 bg-opacity-20': !disabledIcon }"
+                class="transition duration-300 ease-out transform scale-0 rounded-full opacity-0  group-hover:opacity-100 group-focus:opacity-100 group-hover:scale-100 group-focus:scale-100 w-7 h-7"
+                :class="[
+                    !disabledIcon
+                        ? 'bg-transparent bg-opacity-20 ' + bgColor
+                        : '',
+                ]"
             ></div>
         </div>
         <div class="absolute inset-0">
             <div
                 class="flex items-center justify-center w-full h-full text-gray-900 "
-                :class="{
-                    'group-hover:text-blue-600 group-focus:text-blue-600':
-                        !disabledIcon,
-                }"
+                :class="[!disabledIcon ? textColor : '']"
             >
                 <slot></slot>
             </div>
@@ -31,8 +31,24 @@ export default {
     props: {
         disabledIcon: {
             type: Boolean,
-            default: false,
+            default: false
         },
+        color: {
+            type: String,
+            default: "blue"
+        }
     },
+    computed: {
+        bgColor() {
+            return { blue: "bg-blue-600", yellow: "bg-yellow-600" }[this.color];
+        },
+        textColor() {
+            return {
+                blue: "group-hover:text-blue-600 group-focus:text-blue-600",
+                yellow:
+                    "group-hover:text-yellow-600 group-focus:text-yellow-600"
+            }[this.color];
+        }
+    }
 };
 </script>
