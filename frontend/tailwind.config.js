f4dae6a503a6576a7ac103d09d71d6c1a9acbc8a
ByMykel <38622893+ByMykel@users.noreply.github.com>
2021-09-30 19:27:35 +0200

    chore: add transition-width.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     },
     darkMode: false,
     theme: {
-        extend: {},
+        extend: {
+            transitionProperty: {
+                'width': 'width'
+            },
+        },
     },
     variants: {
         extend: {
