e58ba5e84d1afb2eb2eeda8bc5aa989b981dc824
ByMykel <38622893+ByMykel@users.noreply.github.com>
2021-09-30 18:37:45 +0200

    refactor: improve accessibility and design.

--- frontend/src/components/Card/TableForCards.vue
+++ frontend/src/components/Card/TableForCards.vue
@@ -4,7 +4,7 @@
             <tr>
                 <th
                     scope="col"
-                    class="py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase px-7"
+                    class="py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase  px-7"
                 >
                     Question
                 </th>
@@ -30,42 +30,26 @@
                 :class="[wasReviewed(card) ? 'bg-gray-50' : 'bg-white']"
             >
                 <td
-                    class="relative py-4 text-sm font-medium text-gray-900 px-7 whitespace-nowrap"
+                    class="relative py-4 text-sm font-medium text-gray-900  px-7 whitespace-nowrap"
                 >
                     <div
-                        class="absolute inset-0 flex items-center justify-center w-7"
+                        class="absolute inset-0 flex items-center justify-center  w-7"
                     >
-                        <button
-                            v-if="card.favorite"
-                            @click="handleFavorite(card)"
+                        <icon-background
+                            color="yellow"
+                            @click-icon="handleFavorite(card)"
                         >
-                            <svg
-                                class="w-5 h-5 text-yellow-300 cursor-pointer hover:text-yellow-200"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                                ></path>
-                            </svg>
-                        </button>
-                        <button v-else @click="handleFavorite(card)">
-                            <svg
-                                class="w-5 h-5 text-gray-300 cursor-pointer hover:text-yellow-300"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-                                ></path>
-                            </svg>
-                        </button>
+                            <hero-icons-solid
+                                v-if="card.favorite"
+                                class="w-5 h-5 text-yellow-400"
+                                name="star"
+                            ></hero-icons-solid>
+                            <hero-icons-outline
+                                v-else
+                                class="w-5 h-5 text-yellow-400"
+                                name="star"
+                            ></hero-icons-outline>
+                        </icon-background>
                     </div>
                     <action-update-card-question
                         :class="{
@@ -75,7 +59,7 @@
                     ></action-update-card-question>
                 </td>
                 <td
-                    class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap"
+                    class="px-6 py-4 text-sm font-medium text-gray-900  whitespace-nowrap"
                 >
                     <action-update-card-answer
                         :class="{
@@ -93,7 +77,7 @@
                     {{ card.review_date_human }}
                 </td>
                 <td
-                    class="flex items-center justify-end px-6 py-4 text-sm font-medium text-right whitespace-nowrap"
+                    class="flex items-center justify-end px-6 py-4 text-sm font-medium text-right  whitespace-nowrap"
                 >
                     <action-delete-card
                         :card-id="card.id"
@@ -110,6 +94,9 @@ import { mapGetters } from "vuex";
 import repository from "../../api/repository";
 import router from "../../router";
 import store from "../../store";
+import HeroIconsOutline from "../HeroIconsOutline.vue";
+import HeroIconsSolid from "../HeroIconsSolid.vue";
+import IconBackground from "../IconBackground.vue";
 import ActionDeleteCard from "./ActionDeleteCard.vue";
 import ActionUpdateCardAnswer from "./ActionUpdateCardAnswer.vue";
 import ActionUpdateCardQuestion from "./ActionUpdateCardQuestion.vue";
@@ -119,9 +106,12 @@ export default {
         ActionDeleteCard,
         ActionUpdateCardQuestion,
         ActionUpdateCardAnswer,
+        HeroIconsSolid,
+        HeroIconsOutline,
+        IconBackground
     },
     computed: {
-        ...mapGetters("card", ["getCards", "getCardsDeck"]),
+        ...mapGetters("card", ["getCards", "getCardsDeck"])
     },
     methods: {
         wasReviewed(card) {
@@ -149,7 +139,7 @@ export default {
                 .then(() =>
                     store.dispatch("card/fetchCards", { id: deckId, page })
                 );
-        },
-    },
+        }
+    }
 };
 </script>
