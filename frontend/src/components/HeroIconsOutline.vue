6a2c33e17f45741739b37169753f7e79699fc881
ByMykel <38622893+ByMykel@users.noreply.github.com>
2021-10-01 20:13:53 +0200

    feat: add new icons.

--- frontend/src/components/HeroIconsOutline.vue
+++ frontend/src/components/HeroIconsOutline.vue
@@ -68,6 +68,14 @@
             stroke-width="2"
             d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
         ></path>
+        <!-- Icon: x -->
+        <path
+            v-if="name === 'x'"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M6 18L18 6M6 6l12 12"
+        ></path>
     </svg>
 </template>
 
