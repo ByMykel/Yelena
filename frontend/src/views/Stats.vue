dca7dc1f1aca0bbd7e0f1a6c4019d70b8039d529
ByMykel <38622893+ByMykel@users.noreply.github.com>
2021-10-03 19:27:33 +0200

    refactor: add charts to the Stats view.

--- frontend/src/views/Stats.vue
+++ frontend/src/views/Stats.vue
@@ -3,15 +3,60 @@
         <div class="mb-10">
             <h1 class="text-4xl font-bold text-white">Statistics</h1>
         </div>
-        <div
-            class="pb-48 overflow-hidden bg-white border-b border-gray-200 shadow  sm:rounded-lg"
-        ></div>
+        <div class="space-y-4">
+            <div
+                class="overflow-hidden bg-white border-b border-gray-200 shadow  sm:rounded-lg"
+            >
+                <div
+                    class="px-4 py-3 font-medium text-gray-700 border-b border-gray-200  bg-gray-50 sm:px-6"
+                >
+                    Created cards this week
+                </div>
+                <div class="p-2 px-4 sm:px-6">
+                    <created-cards-week
+                        :data="getCardStats.CreatedCardsWeekly"
+                    ></created-cards-week>
+                </div>
+            </div>
+            <div
+                class="space-y-4 overflow-hidden text-gray-700 bg-white border-b border-gray-200 shadow  sm:rounded-lg"
+            >
+                <div
+                    class="px-4 py-3 font-medium border-b border-gray-200  bg-gray-50 sm:px-6"
+                >
+                    Studied cards by quality
+                </div>
+                <div class="p-2 px-4 sm:px-6">
+                    <studied-cards-quality
+                        :data="getCardStats.StudiedCardsByQuality"
+                    ></studied-cards-quality>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import CreatedCardsWeek from "../components/Charts/CreatedCardsWeek.vue";
+import StudiedCardsQuality from "../components/Charts/StudiedCardsQuality.vue";
+import store from "../store";
+
 export default {
-    name: "Home",
-    components: {}
+    name: "Stats",
+    components: { CreatedCardsWeek, StudiedCardsQuality },
+    computed: {
+        ...mapGetters("stats", ["getCardStats"])
+    },
+    beforeRouteEnter(to, from, next) {
+        store.dispatch("stats/fetchCardStats").then(() => {
+            next();
+        });
+    },
+    beforeRouteUpdate(to, from, next) {
+        store.dispatch("stats/fetchCardStats").then(() => {
+            next();
+        });
+    }
 };
 </script>
