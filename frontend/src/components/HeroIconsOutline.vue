edbda9e57759503199b9e4dc1f918b5a5b53d486
Mihail Dragnev <bymykel5@gmail.com>
2021-10-10 20:57:10 +0200

    feat: add new icons.

--- frontend/src/components/HeroIconsOutline.vue
+++ frontend/src/components/HeroIconsOutline.vue
@@ -76,13 +76,37 @@
             stroke-width="2"
             d="M6 18L18 6M6 6l12 12"
         ></path>
+        <!-- Icon: collection -->
+        <path
+            v-if="name === 'collection'"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
+        ></path>
+        <!-- Icon: document-text -->
+        <path
+            v-if="name === 'document-text'"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+        ></path>
+        <!-- Icon: menu -->
+        <path
+            v-if="name === 'menu'"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h16"
+        ></path>
     </svg>
 </template>
 
 <script>
 export default {
     props: {
-        name: String
-    }
+        name: String,
+    },
 };
 </script>
