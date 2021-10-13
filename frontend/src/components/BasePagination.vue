36740d9dcf895bccc05928a15d5309c34a967b10
ByMykel <bymykel5@gmail.com>
2021-10-13 21:59:02 +0200

    refactor: replace icons.

--- frontend/src/components/BasePagination.vue
+++ frontend/src/components/BasePagination.vue
@@ -37,18 +37,10 @@
                         @click="toPage(previousPage)"
                     >
                         <span class="sr-only">Previous</span>
-                        <svg
+                        <hero-icons-solid
                             class="w-5 h-5"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                fill-rule="evenodd"
-                                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                clip-rule="evenodd"
-                            ></path>
-                        </svg>
+                            name="chevron-left"
+                        ></hero-icons-solid>
                     </button>
                     <button
                         v-for="(page, index) in pages"
@@ -66,18 +58,10 @@
                         @click="toPage(nextPage)"
                     >
                         <span class="sr-only">Next</span>
-                        <svg
+                        <hero-icons-solid
                             class="w-5 h-5"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                fill-rule="evenodd"
-                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                clip-rule="evenodd"
-                            ></path>
-                        </svg>
+                            name="chevron-right"
+                        ></hero-icons-solid>
                     </button>
                 </nav>
             </div>
@@ -86,7 +70,9 @@
 </template>
 
 <script>
+import HeroIconsSolid from "./HeroIconsSolid.vue";
 export default {
+    components: { HeroIconsSolid },
     props: {
         action: {
             type: String,
