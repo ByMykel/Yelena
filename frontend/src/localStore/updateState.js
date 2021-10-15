dc859a5249d96ef28f8a2d981fcdfd479ccc5f61
ByMykel <bymykel5@gmail.com>
2021-10-15 21:48:14 +0200

    feat: new updateState.js for the local store.

---
+++ frontend/src/localStore/updateState.js
@@ -0,0 +1,32 @@
+import router from "../router";
+import store from "../localStore";
+
+export default function update(currentCardIdRoute = null) {
+    let page = router.currentRoute.params.page || 1;
+
+    if (router.currentRoute.name === "Stats") {
+        store.dispatch("stats/fetchCardStats");
+
+        return;
+    }
+
+    if (router.currentRoute.name === "DeckById") {
+        if (
+            currentCardIdRoute !== null &&
+            currentCardIdRoute !== router.currentRoute.params.id
+        ) {
+            return;
+        }
+
+        store.dispatch("card/fetchCards", {
+            id: router.currentRoute.params.id,
+            page: page,
+        });
+
+        return;
+    }
+
+    if (router.currentRoute.name === "Decks") {
+        store.dispatch("deck/fetchDecks", { page });
+    }
+}
