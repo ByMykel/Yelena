e5dce8c4680993da16e263a3daa6869492c258f1
ByMykel <bymykel5@gmail.com>
2021-10-20 08:12:41 +0200

    refactor: change background page.

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,100 +1,100 @@
 <template>
-    <div class="pb-64 bg-blue-600">
-        <nav class="bg-blue-600">
-            <div class="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <nav>
+        <div class="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div
+                class="relative flex items-center justify-between h-16 border-b border-blue-300 border-opacity-25 "
+            >
                 <div
-                    class="relative flex items-center justify-between h-16 border-b border-blue-300 border-opacity-25 "
+                    class="absolute inset-y-0 left-0 flex items-center sm:hidden"
+                >
+                    <!-- Mobile menu button-->
+                    <button
+                        type="button"
+                        class="inline-flex items-center justify-center p-2 text-gray-400 rounded-md outline-none hover:text-white hover:bg-blue-500"
+                        aria-controls="mobile-menu"
+                        aria-expanded="false"
+                        @click="showMobileMenu = !showMobileMenu"
+                    >
+                        <span class="sr-only">Open main menu</span>
+                        <hero-icons-outline
+                            class="w-6 h-6"
+                            :class="[showMobileMenu ? 'hidden' : 'block']"
+                            name="menu"
+                        ></hero-icons-outline>
+                        <hero-icons-outline
+                            class="w-6 h-6"
+                            :class="[showMobileMenu ? 'block' : 'hidden']"
+                            name="x"
+                        ></hero-icons-outline>
+                    </button>
+                </div>
+                <div
+                    class="flex items-center justify-start flex-1 ml-12 sm:ml-0 sm:items-stretch"
                 >
                     <div
-                        class="absolute inset-y-0 left-0 flex items-center sm:hidden"
+                        class="flex items-center text-3xl font-bold text-white"
                     >
-                        <!-- Mobile menu button-->
-                        <button
-                            type="button"
-                            class="inline-flex items-center justify-center p-2 text-gray-400 rounded-md outline-none hover:text-white hover:bg-blue-500"
-                            aria-controls="mobile-menu"
-                            aria-expanded="false"
-                            @click="showMobileMenu = !showMobileMenu"
-                        >
-                            <span class="sr-only">Open main menu</span>
-                            <hero-icons-outline
-                                class="w-6 h-6"
-                                :class="[showMobileMenu ? 'hidden' : 'block']"
-                                name="menu"
-                            ></hero-icons-outline>
-                            <hero-icons-outline
-                                class="w-6 h-6"
-                                :class="[showMobileMenu ? 'block' : 'hidden']"
-                                name="x"
-                            ></hero-icons-outline>
-                        </button>
+                        YELENA
                     </div>
-                    <div
-                        class="flex items-center justify-start flex-1 ml-12 sm:ml-0 sm:items-stretch"
-                    >
-                        <div
-                            class="flex items-center text-3xl font-bold text-white "
-                        >
-                            YELENA
+                    <div class="hidden sm:block sm:ml-8">
+                        <div class="flex space-x-4">
+                            <router-link
+                                v-for="(link, index) in links"
+                                :key="index"
+                                :to="link.to"
+                                class="block px-3 py-2 font-medium text-white rounded-md "
+                                :class="
+                                    $route.name === link.route_name
+                                        ? 'bg-blue-700 text-white'
+                                        : 'text-gray-300 hover:bg-blue-500 hover:text-white outline-none focus:bg-blue-500 focus:text-white'
+                                "
+                                >{{ link.name }}</router-link
+                            >
                         </div>
-                        <div class="hidden sm:block sm:ml-8">
-                            <div class="flex space-x-4">
-                                <router-link
-                                    v-for="(link, index) in links"
-                                    :key="index"
-                                    :to="link.to"
-                                    class="block px-3 py-2 font-medium text-white rounded-md "
-                                    :class="
-                                        $route.name === link.route_name
-                                            ? 'bg-blue-700 text-white'
-                                            : 'text-gray-300 hover:bg-blue-500 hover:text-white'
-                                    "
-                                    >{{ link.name }}</router-link
-                                >
-                            </div>
-                        </div>
-                    </div>
-                    <div
-                        class="absolute inset-y-0 right-0 flex items-center pr-2 space-x-1 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
-                    >
-                        <button
-                            v-for="(action, index) in actions"
-                            :key="index"
-                            type="button"
-                            class="p-2 text-white rounded-full outline-none hover:bg-blue-500 focus:bg-blue-500"
-                            @click="openModal(action.modal)"
-                        >
-                            <hero-icons-outline
-                                :class="action.class"
-                                :name="action.icon"
-                            ></hero-icons-outline>
-                        </button>
                     </div>
                 </div>
-            </div>
-
-            <!-- Mobile menu, show/hide based on menu state. -->
-            <div v-show="showMobileMenu" class="sm:hidden" id="mobile-menu">
-                <div class="px-2 pt-2 pb-3 space-y-1">
-                    <router-link
-                        v-for="(link, index) in links"
+                <div
+                    class="absolute inset-y-0 right-0 flex items-center pr-2 space-x-1 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+                >
+                    <button
+                        v-for="(action, index) in actions"
                         :key="index"
-                        :to="link.to"
-                        class="block px-3 py-2 font-medium text-white rounded-md "
-                        :class="
-                            $route.name === link.route_name
-                                ? 'bg-blue-700 text-white'
-                                : 'text-gray-300 hover:bg-blue-500 hover:text-white'
-                        "
-                        >{{ link.name }}</router-link
+                        type="button"
+                        class="p-2 text-white rounded-full outline-none hover:bg-blue-500 focus:bg-blue-500"
+                        @click="openModal(action.modal)"
                     >
-                    <div
-                        class="h-2 border-b border-blue-300 border-opacity-25"
-                    ></div>
+                        <hero-icons-outline
+                            :class="action.class"
+                            :name="action.icon"
+                        ></hero-icons-outline>
+                    </button>
                 </div>
             </div>
-        </nav>
-    </div>
+        </div>
+
+        <!-- Mobile menu, show/hide based on menu state. -->
+        <div v-show="showMobileMenu" class="sm:hidden" id="mobile-menu">
+            <div
+                class="absolute w-full px-2 pt-2 pb-3 space-y-1 bg-blue-600 shadow rounded-b-md"
+            >
+                <router-link
+                    v-for="(link, index) in links"
+                    :key="index"
+                    :to="link.to"
+                    class="block px-3 py-2 font-medium text-white rounded-md"
+                    :class="
+                        $route.name === link.route_name
+                            ? 'bg-blue-700 text-white'
+                            : 'text-gray-300 hover:bg-blue-500 hover:text-white'
+                    "
+                    >{{ link.name }}</router-link
+                >
+                <div
+                    class="h-2 border-b border-blue-300 border-opacity-25"
+                ></div>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>
--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -10,8 +10,41 @@
             :show="getModalVisibility('import_card')"
         ></modal-import-cards>
         <navbar></navbar>
-        <div class="mx-auto my-10 -mt-56 max-w-7xl sm:px-6 lg:px-8">
+        <div class="mx-auto mt-8 mb-20 max-w-7xl sm:px-6 lg:px-8">
             <slot></slot>
+        </div>
+        <!-- Page background -->
+        <div class="fixed top-0 w-full bg-blue-600 -z-10 h-80"></div>
+        <svg
+            class="fixed w-full h-5 text-blue-600 fill-current top-80 -z-10"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+        >
+            <path
+                d="M321.39 56.44c58-10.79 114.16-30.13 172-41.86 82.39-16.72 168.19-17.73 250.45-.39C823.78 31 906.67 72 985.66 92.83c70.05 18.48 146.53 26.09 214.34 3V0H0v27.35a600.21 600.21 0 00321.39 29.09z"
+            ></path>
+        </svg>
+        <div
+            class="fixed w-full text-center text-gray-500 select-none bottom-5 -z-10"
+        >
+            <a
+                class="outline-none group"
+                href="https://github.com/ByMykel"
+                target="_blank"
+            >
+                Made with
+                <span
+                    class="text-sm text-transparent"
+                    style="text-shadow: 0 0 0 rgb(239, 68, 68)"
+                    >❤️</span
+                >
+                by
+                <span
+                    class="group-hover:text-blue-600 group-focus:text-blue-600"
+                    >ByMykel</span
+                >
+            </a>
         </div>
     </div>
 </template>
