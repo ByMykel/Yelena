7abe57aedd99b382d16c0a33695f55c2744d2ea8
ByMykel <bymykel5@gmail.com>
2021-11-01 00:15:38 +0100

    fix: fix pie chart error.

--- frontend/src/components/Charts/StudiedCardsQuality.vue
+++ frontend/src/components/Charts/StudiedCardsQuality.vue
@@ -16,9 +16,7 @@
                     class="block w-3 h-3 mr-1 rounded-full"
                     :style="{ background: item.color }"
                 ></span>
-                <span
-                    class="space-x-1 font-semibold text-gray-400 "
-                >
+                <span class="space-x-1 font-semibold text-gray-400">
                     <span class="text-gray-500">{{ item.cards_count }}</span>
                     <span>{{ item.quality }}</span>
                 </span>
@@ -30,7 +28,7 @@
 <script>
 export default {
     props: {
-        data: Array
+        data: Array,
     },
     data() {
         return {
@@ -38,8 +36,8 @@ export default {
                 Incorrect: "#EF4444",
                 Hard: "#F59E0B",
                 Good: "#3B82F6",
-                Easy: "#10B981"
-            }
+                Easy: "#10B981",
+            },
         };
     },
     computed: {
@@ -51,15 +49,15 @@ export default {
                     cards_count: this.data.reduce(
                         (a, b) => a + b.cards_count,
                         0
-                    )
-                }
+                    ),
+                },
             ];
 
             for (let item of [...this.data]) {
                 data.push({
                     quality: item.quality,
                     color: this.colors[item.quality],
-                    cards_count: item.cards_count
+                    cards_count: item.cards_count,
                 });
             }
 
@@ -70,21 +68,31 @@ export default {
             const sortedData = data.sort((a, b) => {
                 return a.cards_count - b.cards_count;
             });
-            const maxData = Math.max(...data.map(item => item.cards_count));
+            const totalCards = data.reduce(
+                (total, count) => total + count.cards_count,
+                0
+            );
 
             let conicGradient = [];
 
-            for (let item of sortedData) {
-                let color = this.colors[item.quality];
+            if (totalCards) {
+                for (let item of sortedData) {
+                    let color = this.colors[item.quality];
 
-                conicGradient.push(
-                    `${color} 0, ${color} ${(item.cards_count * 100) /
-                        maxData}%`
-                );
+                    conicGradient.push(
+                        `${color} 0, ${color} ${
+                            (item.cards_count * 100) / totalCards
+                        }%`
+                    );
+                }
+            }
+
+            if (!conicGradient.length) {
+                conicGradient = "rgb(17, 24, 39) 0, rgb(17, 24, 39) 100%";
             }
 
             return `background: radial-gradient(circle closest-side, transparent 100%, white 0), conic-gradient(${conicGradient});`;
-        }
-    }
+        },
+    },
 };
 </script>
