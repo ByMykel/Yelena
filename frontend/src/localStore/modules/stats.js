12ea27bb76c02c1d4d68be37171e9d9613ba2ce7
ByMykel <bymykel5@gmail.com>
2022-02-02 18:13:50 +0100

    fix: fix problem with dates.

--- frontend/src/localStore/modules/stats.js
+++ frontend/src/localStore/modules/stats.js
@@ -4,12 +4,13 @@ const currentWeek = () => {
     let date = new Date();
     let firstDay =
         date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1);
-    let lastDay = firstDay + 7;
     let week = [];
 
-    for (; firstDay < lastDay; firstDay++) {
-        date.setDate(firstDay);
+    date.setDate(firstDay);
+
+    for (let i = 0; i < 7; i++) {
         week.push(date.toISOString().slice(0, 10));
+        date.setDate(date.getDate() + 1);
     }
 
     return week;
